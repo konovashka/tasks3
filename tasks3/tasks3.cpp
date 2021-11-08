6e012f6e857037b1fe4d4b08b5a841e047fe6db0
konovashka <anton_konovalov_1@mail.ru>
2021-11-08 23:21:47 +0300

    Добавьте файлы проекта.

---
+++ tasks3/tasks3.cpp
@@ -0,0 +1,27 @@
+﻿//Дано натуральное число, если оно меньше 10 - выдать n! иначе n^k
+
+#include <iostream>
+
+int main()
+{
+    int n, k, m,copy;
+    scanf_s("%d",&k);
+    scanf_s("%d",&n);
+    m = 1;
+    copy = n;
+    if (n<10)
+    {
+        for (int i = 1; i <= k; i++)
+        {
+            m = m * i;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < k; i++)
+        {
+            m = m * copy;
+        }
+    }
+    printf("%d", m);
+}
